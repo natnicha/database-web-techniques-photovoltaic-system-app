78b2619ce94ef2104bad04d8fa993d7ed59ff72d
Natnicha <nat.rodtong@gmail.com>
2023-06-08 21:51:20 +0200

    fix: remove comments

--- web/src/components/BackgroundBase.tsx
+++ web/src/components/BackgroundBase.tsx
@@ -4,11 +4,6 @@ import logo from "../assets/logo.svg"
 
 export function BackgroundBase() {
     return(
-        <div  className="App">
-            {/* <img src={logo} alt="logo" className="mt-16 ml-16 w-40" />
-            <img src={imageLeft} alt="left-financial" className="absolute bottom-0 left-0 w-[30rem]"/>
-            <img src={imageRight} alt="right-financial" className="absolute bottom-0 right-0 w-[30rem]"/> */}
-        </div>
-        
+        <div  className="App"/>
     )
 }
--- web/src/components/Forms/SignIn.tsx
+++ web/src/components/Forms/SignIn.tsx
@@ -34,59 +34,6 @@ export default function SignIn() {
     }
         
     return (
-        // <div className="flex flex-col items-center">
-
-    
-        // <form onSubmit={onSubmit(handleSubmit)} className="flex h-[calc(100vh-95px)] flex-col justify-center items-center outline-none">
-        
-        // <span className="login100-form-title p-b-49">
-		// 				Login
-		// 			</span>
-        // <div className="wrap-input100">
-        // <p className="label-input100">Email</p>
-        
-        // <input
-        //     {...register("email")}
-        //     type="email"
-        //     placeholder="Type your email"
-        //     className={ errors.email ? "block peer rounded-[5px] w-[25rem]  mt-5 border-[#C93B32] focus:outline-none focus:border-[#C93B32]  focus:ring-1 focus:ring-[#C93B32]" : "block peer border-none w-[25rem] mt-5 focus:outline-none"}
-        //     />
-        // <span className="focus-input100" data-symbol="&#xf190;">
-        //     {errors.email?.message}
-        // </span>
-        // </div>
-        // <div className="wrap-input100">
-        //     <p className="label-input100">Password</p>
-        //     <input 
-        //     {...register("password")}
-        //     type="password"
-        //     placeholder="Type your password"
-        //     className={ errors.password ? "input100 block peer rounded-[5px] w-[25rem] mt-5 border-[#C93B32] focus:outline-none focus:border-[#C93B32]  focus:ring-1 focus:ring-[#C93B32]" : "block peer border-none w-[25rem] mt-5 focus:outline-none"}
-        //     />
-        //     </div>
-        // <span className="focus-input100" data-symbol="&#xf190;">
-        //     {errors.password?.message}
-        // </span>
-        // <Link to="/forgotpassword" className="place-self-end hover:text-[#2347C5] hover:underline">
-        // <p className="text-[#5473E3]">Forgot Password?</p>
-        // </Link>
-
-        // <button
-        //     type="submit"
-        //     className={`rounded-full bg-[#3D5FD9] text-[#F5F7FF] w-[25rem] p-3 mt-5 hover:bg-[#2347C5] mb-5`}
-        //     onClick={onHandleSubmit}
-        //     >
-        //     SIGN IN
-        //     </button>
-
-        //     <Link to="/signup" className="hover:text-[#2347C5] hover:underline">
-        //         <p className="text-[#5473E3] mb-5">Don't have an account? Sign up</p>
-        //     </Link>
-        // </form>
-        // </div>
-          
-
-
     <div className="limiter">
 		<div className="container-login100">
 			<div className="wrap-login100 p-l-20 p-r-20 p-b-54">
