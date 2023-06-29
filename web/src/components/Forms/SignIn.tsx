40b18620a64dd5913c1e647b7844f0596ce16387
Natnicha <nat.rodtong@gmail.com>
2023-06-29 19:23:09 +0200

    feat: add feedback when can't to connect to the server

--- web/src/components/Forms/SignIn.tsx
+++ web/src/components/Forms/SignIn.tsx
@@ -70,6 +70,7 @@ export default function SignIn() {
         }
       })
       .catch((error) => {
+        setFeedback("couldn't connect to the server")
         console.log('error: ' + error);
       });
   };
